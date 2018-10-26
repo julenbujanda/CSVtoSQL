bb1b467672530f2530cc54c02a55940c2c1a1b9d
julenb <21701719@live.uem.es>
2018-10-26 17:05:53 +0200

    Mostrar resultados

--- src/CSVtoSQL.java
+++ src/CSVtoSQL.java
@@ -2,11 +2,11 @@ import java.io.*;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Properties;
+import java.util.Scanner;
 
 public class CSVtoSQL {
 
@@ -16,6 +16,7 @@ public class CSVtoSQL {
 
     private static Connection connection;
     private static Properties propiedades;
+    private static Scanner scanner;
 
     public static void crearTablas() {
         // language=SQL
@@ -112,7 +113,6 @@ public class CSVtoSQL {
             }
         }
         queryInserción += ";";
-        System.out.println(queryInserción);
         try {
             connection.prepareStatement(queryInserción).executeUpdate();
         } catch (SQLException e) {
@@ -156,6 +156,62 @@ public class CSVtoSQL {
         }
     }
 
+    public static int mostrarEstaciones() {
+        System.out.println("Escoger estación:");
+        int seleccion = 0;
+        HashMap<Integer, Integer> ids = new HashMap<>();
+        try {
+            ResultSet resultSetEstaciones = connection.prepareStatement(
+                    "SELECT ID, NOMBRE FROM estaciones, contaminacion WHERE estaciones.ID = contaminacion.ESTACION GROUP BY ID;").executeQuery();
+            int num = 0;
+            while (resultSetEstaciones.next()) {
+                System.out.println(++num + ") " + resultSetEstaciones.getString(2));
+                ids.put(num, resultSetEstaciones.getInt(1));
+            }
+            seleccion = scanner.nextInt();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ids.get(seleccion);
+    }
+
+    public static int mostrarContaminantes(int estacion) {
+        System.out.println("Escoger contaminante:");
+        int seleccion = 0;
+        HashMap<Integer, Integer> ids = new HashMap<>();
+        try {
+            ResultSet resultSetContaminantes = connection.prepareStatement(
+                    "SELECT ID, NOMBRE FROM contaminantes, contaminacion WHERE contaminacion.MAGNITUD = contaminantes.ID AND ESTACION = " + estacion + " GROUP BY ID;").executeQuery();
+            int num = 0;
+            while (resultSetContaminantes.next()) {
+                System.out.println(++num + ") " + resultSetContaminantes.getString(2));
+                ids.put(num, resultSetContaminantes.getInt(1));
+            }
+            seleccion = scanner.nextInt();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return ids.get(seleccion);
+    }
+
+    public static void mostrarResultados(int estacion, int contaminante) {
+        System.out.println("Resultados:");
+        try {
+            ResultSet resultSetDatos = connection.prepareStatement("SELECT H01, V01, H02, V02, H03, V03, H04, V04, H05, V05, H06, V06, H07, V07, H08, V08, H09, V09, H10, V10, H11, V11, H12, V12, H13, V13, H14, V14, H15, V15, H16, V16, H17, V17, H18, V18, H19, V19, H20, V20, H21, V21, H22, V22, H23, V23, H24, V24 FROM contaminacion WHERE MAGNITUD = " + contaminante + " AND ESTACION = " + estacion + ";").executeQuery();
+            ResultSetMetaData resultSetMetaData = resultSetDatos.getMetaData();
+            int numColumnas = resultSetMetaData.getColumnCount();
+            String salida = "";
+            while (resultSetDatos.next()) {
+                for (int i = 1; i <= numColumnas; i++) {
+                    salida += resultSetMetaData.getColumnName(i) + ": " + resultSetDatos.getString(i) + "\n";
+                }
+            }
+            System.out.println(salida);
+        } catch (SQLException e) {
+
+        }
+    }
+
     public static void main(String[] args) {
         propiedades = new Properties();
         leerTablas();
@@ -172,6 +228,10 @@ public class CSVtoSQL {
         insertarEstaciones();
         insertarContaminantes();
         insertarDatos();
+        scanner = new Scanner(System.in);
+        int estacion = mostrarEstaciones();
+        int contaminante = mostrarContaminantes(estacion);
+        mostrarResultados(estacion, contaminante);
     }
 
 }
